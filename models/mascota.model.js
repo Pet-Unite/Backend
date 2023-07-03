b07150f7040a3467bb939ab77c146dabf3151599
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-07-03 15:06:51 -0500

    Actualizar valores permitidos en mascotamodel tipo

--- models/mascota.model.js
+++ models/mascota.model.js
@@ -1,17 +1,47 @@
-const mongoose =require("mongoose");
+const mongoose = require("mongoose");
 
-const mascotaSchema=mongoose.Schema({
-    tipo:{type:String,require},
-    raza: {type:String,require},
-    src: {type:String,require},
-    longevidad: {type:String,require},
-    origen: {type:String,require},
-    tamaño: {type:String,require},
-    historia: {type:String,require},
-    likes: {type: Number, default: 0},
-    comentarios:{type:Array}
-},{
-    timestamps:true,
-})
-const MascotaModel=mongoose.model('mascotas',mascotaSchema)
-module.exports=MascotaModel
+const mascotaSchema = mongoose.Schema(
+  {
+    tipo: {
+      type: String,
+      require,
+      enum: ["perro", "gato", "pajaro", "roedor"],
+    },
+    raza: {
+      type: String,
+      require,
+    },
+    src: {
+      type: String,
+      require,
+    },
+    longevidad: {
+      type: String,
+      require,
+    },
+    origen: {
+      type: String,
+      require,
+    },
+    tamaño: {
+      type: String,
+      require,
+    },
+    historia: {
+      type: String,
+      require,
+    },
+    likes: {
+      type: Number,
+      default: 0,
+    },
+    comentarios: {
+      type: Array,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const MascotaModel = mongoose.model("mascotas", mascotaSchema);
+module.exports = MascotaModel;

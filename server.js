6cce3bf226abb1663e561ed854237a18511cdbc2
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-07-03 15:47:14 -0500

    Agregar observacion sobre ruta de mascotas

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.use((req, res, next) => {
 
 // middleware de rutas
 app.use("/api/user", usersRoutes);
+//OJO DEBE SER /api/mascotas
 app.use("/", mascotasRoutes);
 app.use("/api/comentarios", comentariosRoutes);
 

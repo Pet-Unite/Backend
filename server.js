04f86cf9207765a8a2761e249a3a44e1be060f66
ElNuVaTo <amaroalcaq23@gmail.com>
2023-07-03 10:12:42 -0400

    feat: Se agrego el paquete cors, PosdData: Instalar de nuevo los modulos

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require ('express');
-const db = require('./config/db')
-
-
+const express = require("express");
+const cors = require("cors");
+const db = require("./config/db");
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
@@ -11,12 +11,11 @@ require("./routes/user.routes")(app);
 require("./routes/mascotas.routes")(app);
 require("./routes/comentarios.routes")(app);
 
-
-app.get("/", (req, res)=>{
-    res.send('Servidor trabajando' + port);
+app.get("/", (req, res) => {
+  res.send("Servidor trabajando" + port);
 });
 app.use(express.static("public"));
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, ()=> 'servidor trabajando en el puerto');
+app.listen(port, () => "servidor trabajando en el puerto");
